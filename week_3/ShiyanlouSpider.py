73e784b4f4ecaaee6a3b2cfefbed073b51da420b
Shouldong <shouldong@gmail.com>
2017-12-05 23:32:19 +0800

    ..

--- week_3/ShiyanlouSpider.py
+++ week_3/ShiyanlouSpider.py
@@ -12,4 +12,9 @@ class ShiyanlouSpider(scrapy.Spider):
 		return (url_tmpl.format(i) for i in range(1, 5))
 
 	def parse(self, response):
-		pass
+		for repository in response.css('li.public'):
+			yield {
+				'name': repository.css('div.mb-1 a::text').extract_first(),
+				'update_time': repository.xpath('.//relative-time/@datetime').extract_first()
+			}
+		

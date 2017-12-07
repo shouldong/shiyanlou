32038187583f87bb88e2dc66b51433f4f723a26c
kid1412hj <kid1412hj@sina.com>
2017-12-07 21:17:08 +0800

    ..

--- week_3/shiyanlougithub/shiyanlougithub/spiders/shiyanlouspider.py
+++ week_3/shiyanlougithub/shiyanlougithub/spiders/shiyanlouspider.py
@@ -15,9 +15,27 @@ class ShiyanlouSpider(scrapy.Spider):
 
 	def parse(self, response):
 		for repository in response.css('li.public'):
-			item = ShiyanlougithubItem({
-				'name': repository.css('div.mb-1 a::text').re_first("\n\s*(.*)"),
-				'update_time': repository.xpath('.//relative-time/@datetime').extract_first()
-			})
-			yield item
+			item = ShiyanlougithubItem()
+			item['name'] = repository.css('div.mb-1 a::text').re_first("\n\s*(.*)")
+			item['update_time'] = repository.xpath('.//relative-time/@datetime').extract_first()
+			detail_url = response.urljoin(repository.xpath('.//a/@href').extract_first())
+			request = scrapy.Request(detail_url, callback=self.parse_detail)
+			request.meta['item'] = item
+			yield request
+
+	def parse_detail(self, response):
+		item = response.meta['item']
+		for detail in response.css('ul.numbers-summary li'):
+			detail_text = detail.xpath('.//a/text()').re_first("\n\s*(.*)\n")
+			detail_num = detail.xpath('.//span[@class="num text-emphasized"]/text()').re_first("\n\s*(.*)\n")
+			if detail_text and detail_num:
+				detail_num = detail_num.replace(',', '')
+				if detail_text in ('commit', 'commits'):
+					item['commits'] = int(detail_num)
+				elif detail_text in ('branch', 'branches'):
+					item['branches'] = int(detail_num)
+				elif detail_text in ('release', 'releases'):
+					item['releases'] = int(detail_num)
+		yield item
+
 		

7885ffff5f1ed50ef9855dcad205fb224c6cdfff
kid1412hj <kid1412hj@sina.com>
2017-12-27 15:27:18 +0800

    ..

---
+++ week_6/ip_handler.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import re
+import os
+from wechatpy.messages import TextMessage
+from wechatpy import create_reply
+from qqwry import QQwry
+
+
+class CommandHandler:
+	command = ''
+
+	def check_match(self, message):
+		if not isinstance(message, TextMessage):
+			return False
+		if not message.content.strip().lower().startswith(self.command):
+			return False
+		return True
+
+
+class IPLocationHandler(CommandHandler):
+	command = 'ip'
+
+	def __init__(self):
+		file = os.environ.get('QQWRY_DAT', 'qqwry.dat')
+		self.q = QQwry()
+		self.q.load_file(file)
+
+	def handle(self, message):
+		if not self.check_match(message):
+			return
+		parts = message.content.strip().split()
+		if len(parts) == 1 or len(parts) > 2:
+			return create_reply('IP地址无效', message)
+		ip = parts[1]
+		pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'
+		if not re.match(pattern, ip):
+			return create_reply('IP地址无效', message)
+		result = self.q.lookup(ip)
+		if result is None:
+			return create_reply('Not found', message)
+		else:
+			return create_reply(result[0], message)
